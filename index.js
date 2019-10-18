a32f5c7be43cdb1165342b9f70665329f9e7aeed
Sandrome <sudayavarman@salesforce.com>
2019-10-18 16:07:58 +0530

    Changes in inde.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const bot = new Discord.Client();
+const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
