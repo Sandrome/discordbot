cc365f370ef37f52e62da2ef45cb18cdd3e6de6f
Sandrome <sudayavarman@salesforce.com>
2019-10-18 16:20:56 +0530

    Changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 var token = process.env.BOT_TOKEN;
-bot.login(token);
+bot.login('NjM0MzAzNjMwNDUwNzUzNTU1.XamRVA.z9GvlN-ap6Dwn-j_FEY5faVftLM');
 
 bot.on('ready', () => {
     console.log(`Logged in as Sreyas!`);
