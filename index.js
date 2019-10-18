9cb262b6949ca0df2c956f8665b8990acdc4b6b1
Sandrome <sudayavarman@salesforce.com>
2019-10-18 15:30:53 +0530

    Commited again.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('I am ready!');
+    console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', message => {
