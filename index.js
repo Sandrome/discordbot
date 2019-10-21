d3e0fff05e63c399eec55b6f375a19ccfcceb4db
Sandrome <sudayavarman@salesforce.com>
2019-10-21 13:21:18 +0530

    Changes

--- index.js
+++ index.js
@@ -2,14 +2,28 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 
 
-bot.on('ready', () => {
+bot.on('ready', function() {
     console.log(`Logged in as Sreyas!`);
 });
 
-bot.on('message', message => {
+bot.on('message', function(user, userID, channelID, message, evt) {
     if (message.content === 'ping') {
        message.reply('pong');
        }
+       if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+        var data = args[1];
+
+        switch(cmd) {
+            case 'ping':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Pong!'+data
+                });
+            break;
+         }
+     }
 });
 
 bot.login(process.env.BOT_TOKEN);
