623d875ccd4706cadd6d9a3093cdd1b66a7e67e5
Sandrome <sudayavarman@salesforce.com>
2019-10-22 12:43:56 +0530

    changes in index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ bot.on('message', msg => {
     if (msg.content === 'ping') {
        msg.reply('pong');
        }
-    else (msg.content === '!level') {
+    else if(msg.content === '!level') {
         msg.reply('Waiting');
     }
        /*if (message.substring(0, 1) == '!') {
