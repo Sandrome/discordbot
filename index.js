99920e01d84124d9b720d3f18f0225c85867da50
Sandrome <sudayavarman@salesforce.com>
2019-10-23 16:42:05 +0530

    More changes in index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+const request = require('request');
 
 bot.on('ready', function() {
     console.log(`Logged in as Sreyas!`);
@@ -14,10 +15,30 @@ bot.on('message', msg => {
             var args = msg.content.substring(1).split(' ');
             var cmd = args[0];
             var data = args[1];
+            var data_encoded = encodeURIComponent(data);
             //msg.channel.send(cmd);
 
         switch(cmd) {
             case 'level':
+                    let url = `https://r6tab.com/api/search.php?platform=uplay&search=${data_encoded}`
+                    request(url, function (err, response, body) {
+                        if(err){
+                          //res.render('player_details', {player: null, error: 'There is some error! Please try again :)'});
+                          msg.channel.send('error occured');
+                        } else {
+                          let player = JSON.parse(body)
+                          if(player.results == undefined){
+                            //res.render('player_details', {player: null, error: 'Error, Player does not exist'});
+                            msg.channel.send('error undefinded');
+                          } else {
+                            let playerText = `It's ${player.results[0].p_name}`;
+                            msg.channel.send(playerText);
+                            //res.render('player_details', {player: playerText, error: null});
+                            //res.json(player);
+                          }
+                        }
+                      });
+                    });
                 msg.channel.send(data);
             break;
          }
