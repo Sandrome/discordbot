79bfd398c8f2b5adf0446aace8dada8f3fa30e48
Sandrome <sudayavarman@salesforce.com>
2019-10-26 22:51:09 +0530

    New technique used

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ bot.on('message', msg => {
 
         switch(cmd) {
             case 'stats:casual':
-                    stats_casual(player_url);
+                    msg.channel.send(stats_casual(player_url));
                 //msg.channel.send(data);
             break;
          }
@@ -29,17 +29,17 @@ bot.on('message', msg => {
 });
 
 //Defined Functions
-function stats_casual(player_url){
-  request(player_url, function (err, response, body) {
+function stats_casual(player_url_f){
+  request(player_url_f, function (err, response, body) {
     if(err){
-        msg.channel.send('Error Retype');
+        return 'Error Retype';
     } else {
       let player = JSON.parse(body);
       if(player.results == undefined){
-        msg.channel.send('It is Undefined');
+        return 'It is Undefined';
       } else {
         let playerText = `Player ID: "${player.results[0].p_id}"`;
-        msg.channel.send(playerText);
+        return playerText;
         //res.json(player);
       }
     }
