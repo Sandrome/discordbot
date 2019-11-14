200145a5b452027207e60578d94c748a35cab9d1
Sandrome <sudayavarman@salesforce.com>
2019-11-14 13:46:05 +0530

    Added multiple request methods

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ bot.on('message', msg => {
             var data = args[1];
             var data_encoded = encodeURIComponent(data);
             let player_url = `https://r6tab.com/api/search.php?platform=uplay&search=${data_encoded}`
-
+            
         switch(cmd) {
-            case 'stats:casual':
+            case 'casual:stats':
                     request(player_url, function (err, response, body) {
                         if(err){
                             msg.channel.send('Error Retype');
@@ -28,13 +28,12 @@ bot.on('message', msg => {
                           if(player.results == undefined){
                             msg.channel.send('It is Undefined');
                           } else {
-                            var playerText = `Player ID: "${player.results[0].p_id}"`;
+                            var playerText = `Player ID: ${player.results[0].p_id}`;
                             msg.channel.send(playerText);
-                            //res.json(player);
                           }
                         }
                       });
-            case 'stats:mmr':
+            case 'ranked:stats':
                 request(player_url, function (err, response, body) {
                   if(err){
                       msg.channel.send('Error Retype');
@@ -43,9 +42,24 @@ bot.on('message', msg => {
                     if(player.results == undefined){
                       msg.channel.send('It is Undefined');
                     } else {
-                      var playerText = `Player ID: "${player.results[0].p_currentmmr}"`;
-                      msg.channel.send(playerText);
-                      //res.json(player);
+                      var playerid = `Ranked MMR: ${player.results[0].p_id}`;
+                      msg.channel.send(playerid);
+                      var data_encoded_ranked = encodeURIComponent(playerid);
+                      let player_url_ranked = `https://r6tab.com/api/player.php?p_id=${data_encoded_ranked}`
+
+                      request(player_url_ranked, function (err, response, body) {
+                        if(err){
+                            msg.channel.send('Error Retype');
+                        } else {
+                          let player_ranked = JSON.parse(body);
+                          if(player.results == undefined){
+                            msg.channel.send('It is Undefined');
+                          } else {
+                            var playerText = `Ranked Wins: ${player_ranked.matches[0].db_p_total_rankedwins}`;
+                            msg.channel.send(playerText);
+                          }
+                        }
+                      });
                     }
                   }
                 });
